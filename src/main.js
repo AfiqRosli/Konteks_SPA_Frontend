02c245d5f7fff345293d52dc77bce0fa5e131f1e
Afiq <afiq_rosli@live.com>
2020-08-24 12:04:46 +0800

    dynmically add vue comp

--- src/main.js
+++ src/main.js
@@ -9,6 +9,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all";
 import "./assets/scss/app.scss";
 
+const requireComponent = require.context(
+    './components',
+    false,
+    /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName)
+
+    const componentName = upperFirst(
+        camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+    )
+
+    Vue.component(componentName, componentConfig.default || componentConfig)
+})
+
 Vue.config.productionTip = false;
 
 new Vue({

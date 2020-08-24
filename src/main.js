a22e10edf6e1ad7c1c62a0b83c6fb8ad46e63672
Afiq <afiq_rosli@live.com>
2020-08-24 10:31:11 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all";
+import "./assets/scss/app.scss";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Login</h1>
+
+    <form @submit.prevent="login">
+      <div>
+        <input type="email" />
+      </div>
+
+      <div>
+        <input type="password" />
+      </div>
+
+      <button>Login</button>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8000";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    login() {
+      axios.get("/sanctum/csrf-cookie").then((res) => {
+        axios
+          .post("/login", {
+            email: "asdf@live.com",
+            password: "password",
+          })
+          .then((res) => {
+            console.log(res);
+          });
+      });
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+</style>

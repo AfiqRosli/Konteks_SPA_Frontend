e9ae8a323d8f72514288ae3f8ea7fda47cd6caf2
Afiq <afiq_rosli@live.com>
2020-08-26 14:26:34 +0800

    init protected route

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from "@/store/index.js";
+
 import Home from '../views/Home.vue'
 import Topic from '../views/Topic.vue'
 
@@ -15,31 +17,47 @@ const routes = [{
     name: 'Home',
     component: Home,
     meta: {
-      title: 'Learn Languages'
-    }
+      title: 'Learn Languages',
+    },
   },
   {
     path: '/topic',
     name: 'Topic',
     component: Topic,
     meta: {
-      title: 'Start a Topic'
-    }
-  }
+      title: 'Start a Topic',
+      requiresAuth: true,
+    },
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 const DEFAULT_TITLE = 'Konteks'
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters['auth/authenticated']) {
+      next()
+    } else {
+      next({
+        name: 'Home'
+      })
+    }
+  } else {
+    next()
+  }
+})
+
 router.afterEach((to, from) => {
   Vue.nextTick(() => {
-    document.title = to.meta.title || DEFAULT_TITLE;
-  });
-});
+    document.title = to.meta.title || DEFAULT_TITLE
+  })
+})
+
 
 export default router

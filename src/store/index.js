b7122337bb08174835ee9aea645c8b0c456238c9
Afiq <afiq_rosli@live.com>
2020-08-28 00:55:54 +0800

    import topic module to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import * as auth from './module/auth'
+import * as topic from './module/topic'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    topic
   }
 })

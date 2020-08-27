3fc5443e0461eb159c76fa48cc7551299044befd
Afiq <afiq_rosli@live.com>
2020-08-28 00:57:21 +0800

    learnLanguage method init push to Topic component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,7 @@
                     >
                         <div class="mx-4 mb-5">
                             <img
+                                @click="learnLanguage"
                                 class="img_pointer img_size_medium"
                                 src="@/assets/img/country-flag/brunei-flag-round-medium.png"
                                 alt="brunei-flag-round-medium"
@@ -55,12 +56,12 @@
 </template>
 
 <script>
-import LoginModal from "@/components/modals/LoginModal";
-import RegisterModal from "@/components/modals/RegisterModal";
-import axios from "axios";
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
+import axios from 'axios'
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:8000'
 
 export default {
     components: {
@@ -69,19 +70,22 @@ export default {
     },
     methods: {
         login() {
-            axios.get("/sanctum/csrf-cookie").then((res) => {
+            axios.get('/sanctum/csrf-cookie').then(res => {
                 axios
-                    .post("/login", {
-                        email: "asdf@live.com",
-                        password: "password",
+                    .post('/login', {
+                        email: 'asdf@live.com',
+                        password: 'password',
                     })
-                    .then((res) => {
-                        console.log(res);
-                    });
-            });
+                    .then(res => {
+                        console.log(res)
+                    })
+            })
+        },
+        learnLanguage() {
+            this.$router.push({ name: 'Topic' })
         },
     },
-};
+}
 </script>
 
 <style lang="scss" scoped></style>

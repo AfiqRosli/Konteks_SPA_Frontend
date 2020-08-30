bcd12a2dd6f95be1a8836b79d943c0a4ed465a63
Afiq <afiq_rosli@live.com>
2020-08-30 16:56:07 +0800

    Topic/Show.vue init

---
+++ src/views/Topic/Show.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="pt-5 mt-5">{{ this.topic }}</div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    props: ['id'],
+    computed: {
+        ...mapState('topic', ['topic']),
+    },
+    created() {
+        this.$store.dispatch('topic/fetchTopic', this.id)
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>

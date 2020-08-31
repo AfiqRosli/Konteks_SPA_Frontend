a975d92bcf1f305e2a1984e0985c23495b83862e
Afiq <afiq_rosli@live.com>
2020-08-31 08:51:17 +0800

    if logged in; logo redirect to TopicIndex

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from "@/store/index.js";
+import store from '@/store/index.js'
 
 import Home from '../views/Home.vue'
 import TopicIndex from '../views/Topic/Index.vue'
@@ -13,62 +13,71 @@ import TopicShow from '../views/Topic/Show.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home,
-    meta: {
-      title: 'Learn Languages',
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta: {
+            title: 'Learn Languages',
+        },
+        beforeEnter: (to, from, next) => {
+            if (store.getters['auth/authenticated']) {
+                next({
+                    name: 'TopicIndex',
+                })
+            } else {
+                next()
+            }
+        },
     },
-  },
-  {
-    path: '/topic',
-    name: 'TopicIndex',
-    component: TopicIndex,
-    meta: {
-      title: 'Start a Topic',
-      requiresAuth: true,
+    {
+        path: '/topic',
+        name: 'TopicIndex',
+        component: TopicIndex,
+        meta: {
+            title: 'Start a Topic',
+            requiresAuth: true,
+        },
     },
-  },
-  {
-    path: '/topic/:id',
-    name: 'TopicShow',
-    component: TopicShow,
-    meta: {
-      title: 'Learning by Guessing',
-      requiresAuth: true,
+    {
+        path: '/topic/:id',
+        name: 'TopicShow',
+        component: TopicShow,
+        meta: {
+            title: 'Learning by Guessing',
+            requiresAuth: true,
+        },
+        props: true,
     },
-    props: true
-  }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes,
 })
 
 const DEFAULT_TITLE = 'Konteks'
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (store.getters['auth/authenticated']) {
-      next()
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters['auth/authenticated']) {
+            next()
+        } else {
+            next({
+                name: 'Home',
+            })
+        }
     } else {
-      next({
-        name: 'Home'
-      })
+        next()
     }
-  } else {
-    next()
-  }
 })
 
 router.afterEach((to, from) => {
-  Vue.nextTick(() => {
-    document.title = to.meta.title || DEFAULT_TITLE
-  })
+    Vue.nextTick(() => {
+        document.title = to.meta.title || DEFAULT_TITLE
+    })
 })
-
 
 export default router

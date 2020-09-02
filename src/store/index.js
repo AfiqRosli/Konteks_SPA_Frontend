129108da1714a90279d52858696b9c90833ee937
Afiq <afiq_rosli@live.com>
2020-09-02 10:33:27 +0800

    import lesson module

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as auth from './module/auth'
 import * as topic from './module/topic'
+import * as lesson from './module/lesson'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    auth,
-    topic
-  }
+    modules: {
+        auth,
+        topic,
+        lesson,
+    },
 })

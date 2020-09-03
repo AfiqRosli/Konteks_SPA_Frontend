dbbb46ee0a7a422b82e7875ba139f79cd67a482b
Afiq <afiq_rosli@live.com>
2020-09-03 12:03:22 +0800

    lesson_content module init

---
+++ src/store/module/lesson_content.js
@@ -0,0 +1,27 @@
+import LessonContentService from '@/services/LessonContentService.js'
+
+export const namespaced = true
+
+export const state = {
+    lesson_contents: [],
+}
+
+export const mutations = {
+    SET_LESSON_CONTENTS(state, lesson_contents) {
+        state.lesson_contents = lesson_contents
+    },
+}
+
+export const actions = {
+    async fetchLessonContents({ commit }, lesson) {
+        try {
+            let res = await LessonContentService.getLessonContents(lesson)
+            console.log(res.data)
+            commit('SET_LESSON_CONTENTS', res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    },
+}
+
+export const getters = {}

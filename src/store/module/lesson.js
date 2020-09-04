d956d26aacab4178bd70e396971326299c26144f
Afiq <afiq_rosli@live.com>
2020-09-05 00:32:46 +0800

    lesson module add lesson state, mut, act

--- src/store/module/lesson.js
+++ src/store/module/lesson.js
@@ -4,11 +4,15 @@ export const namespaced = true
 
 export const state = {
     lessons: [],
+    lesson: null,
 }
 
 export const mutations = {
     SET_LESSONS(state, lessons) {
         state.lessons = lessons
+    },
+    SET_LESSON(state, lesson) {
+        state.lesson = lesson
     },
 }
 
@@ -17,6 +21,14 @@ export const actions = {
         try {
             let res = await LessonService.getTopicLessons(topic_id)
             commit('SET_LESSONS', res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    },
+    async fetchLesson({ commit }, lesson_id) {
+        try {
+            let res = await LessonService.getLesson(lesson_id)
+            commit('SET_LESSON', res.data)
         } catch (err) {
             console.log(err)
         }

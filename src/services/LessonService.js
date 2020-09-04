218ef9ea8a5203040536f2b33931b8f8811d4e24
Afiq <afiq_rosli@live.com>
2020-09-05 00:32:17 +0800

    LessonService apiClient getLesson

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -4,4 +4,7 @@ export default {
     getTopicLessons(topic) {
         return apiClient.get(`/topics/${topic}/lessons`)
     },
+    getLesson(lesson) {
+        return apiClient.get(`/lessons/${lesson}`)
+    }
 }

f3f6ca6c1ac2b80c146f105a820b83c2f55f9498
Afiq <afiq_rosli@live.com>
2020-09-05 00:31:53 +0800

    add slash to getLessonContents API

--- src/services/LessonContentService.js
+++ src/services/LessonContentService.js
@@ -2,6 +2,6 @@ import apiClient from './API'
 
 export default {
     getLessonContents(lesson) {
-        return apiClient.get(`lessons/${lesson}/lesson_contents`)
+        return apiClient.get(`/lessons/${lesson}/lesson_contents`)
     }
 }

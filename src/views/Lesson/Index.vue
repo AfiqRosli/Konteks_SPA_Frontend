4235a793be85204a19b2c729767b7db79bf41ffe
Afiq <afiq_rosli@live.com>
2020-09-09 11:43:03 +0800

    npm tiny-tfidf and use cosine similarity

--- src/views/Lesson/Index.vue
+++ src/views/Lesson/Index.vue
@@ -9,14 +9,9 @@
                             params: { id: this.lesson.topic_id },
                         }"
                         class="breadcrumb__link"
-                        >{{
-                            this.capitalizePhrase(this.topic.title)
-                        }}</router-link
-                    >
+                    >{{ this.capitalizePhrase(this.topic.title) }}</router-link>
                 </li>
-                <li class="breadcrumb-item active" aria-current="page">
-                    {{ this.lesson.title }}
-                </li>
+                <li class="breadcrumb-item active" aria-current="page">{{ this.lesson.title }}</li>
             </ol>
         </nav>
 
@@ -27,15 +22,9 @@
 
         <div style="height: 300px"></div>
 
-        <div
-            class="row mb-5"
-            v-for="lesson_content in lesson_contents"
-            :key="lesson_content.id"
-        >
+        <div class="row mb-5" v-for="lesson_content in lesson_contents" :key="lesson_content.id">
             <div class="col-12 col-sm-2 text-left text-sm-right order-1">
-                <span v-if="!lesson_content.user_input">{{
-                    lesson_content.name
-                }}</span>
+                <span v-if="!lesson_content.user_input">{{ lesson_content.name }}</span>
             </div>
             <div class="col-12 col-sm-8 order-0 order-sm-1 mb-3 mb-sm-0">
                 <div
@@ -46,9 +35,7 @@
                     <p
                         v-if="!lesson_content.user_input"
                         class="lesson-content__translation m-0"
-                    >
-                        {{ lesson_content.says_translation }}
-                    </p>
+                    >{{ lesson_content.says_translation }}</p>
 
                     <input
                         v-else
@@ -59,24 +46,21 @@
                     />
                 </div>
             </div>
-            <div
-                class="col-12 col-sm-2 text-right text-sm-left order-2 order-sm-2"
-            >
-                <span v-if="lesson_content.user_input">{{
-                    lesson_content.name
-                }}</span>
+            <div class="col-12 col-sm-2 text-right text-sm-left order-2 order-sm-2">
+                <span v-if="lesson_content.user_input">{{ lesson_content.name }}</span>
             </div>
         </div>
 
         <div class="row mb-5">
             <div class="col-8 offset-2 text-right">
-                <button class="btn btn-primary">SUBMIT</button>
+                <button class="btn btn-primary" @click="checkSimilarity">SUBMIT</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { Corpus, Similarity } from 'tiny-tfidf'
 import { mapState, mapActions } from 'vuex'
 import helper from '@/mixins/helper'
 
@@ -106,6 +90,31 @@ export default {
             return {
                 'lesson-content__speech_left': !reqUserInput,
                 'lesson-content__speech_right': reqUserInput,
+            }
+        },
+        checkSimilarity() {
+            // d1: document 1 etc.
+
+            console.log(this.lesson_contents)
+
+            // TODO: Test user input with all documents and get the highest similarity
+            const corpus = new Corpus(
+                ['d1', 'd2'],
+                [
+                    'Aku keraja sebagai tukang gambar professional.',
+                    'Aku ani bekeraja sebagai tukang gambar professional',
+                ]
+            )
+
+            // console.log(
+            //     Similarity.cosineSimilarity(
+            //         corpus.getDocumentVector('d1'),
+            //         corpus.getDocumentVector('d2')
+            //     )
+            // )
+
+            for (let i = 1; i <= corpus._documents.size; i++) {
+                console.log(corpus.getDocument('d' + i))
             }
         },
     },
